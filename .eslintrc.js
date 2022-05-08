408565387a938b281e5bd8efacbba0544a5befaa
Malchevsky Alexander <malickalex1975@gmail.com>
2022-05-08 16:24:08 +0300

    fix:fix with eslint-confif-airbnb-base

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
+    'root':true,
     "env": {
         "browser": true,
         "es2021": true
     },
     "extends": [
-        "eslint:recommended"
+        "airbnb-base"
         
     ],
     "parserOptions": {
